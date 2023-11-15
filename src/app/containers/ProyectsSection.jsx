d4b3368ec6a752aec138d878d5e7cdec4a49080e
asdjosegarcia <asdjosegarcia@gmail.com>
2023-11-15 10:09:50 -0300

    fix svg  warnings

---
+++ src/app/containers/ProyectsSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ProyectsSection.css'  
+
+const ProyectsSection = () => {
+  return (
+    <div className='proyectsSection__container'>
+      <h2>Proyectos Personales</h2>
+      
+    </div>
+  )
+}
+
+export default ProyectsSection
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,14 @@
 import 'normalize.css'
 import MainSection from './containers/MainSection'
 import AboutSection from './containers/AboutSection'
-
+import ProyectsSection from './containers/ProyectsSection'
 
 export default function Home() {
   return (
     <>
-
         <MainSection></MainSection>
         <AboutSection></AboutSection>
+        <ProyectsSection></ProyectsSection>
     </>
   )
 }
