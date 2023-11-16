9f8d36708fa45d97543fd56dfef5a4c9848e823f
asdjosegarcia <asdjosegarcia@gmail.com>
2023-11-16 02:51:19 -0300

    add partial prects-section

--- src/app/containers/ProyectsSection.jsx
+++ src/app/containers/ProyectsSection.jsx
@@ -3,10 +3,21 @@ import './ProyectsSection.css'
 
 const ProyectsSection = () => {
   return (
-    <div className='proyectsSection__container'>
-      <h2>Proyectos Personales</h2>
-      
-    </div>
+    <section className='proyectsSection__container'>
+      <div className='proyects__container'>
+      <h2 className='proyects__title'>Proyectos Personales</h2>
+      <div className='proyects__cards__container'>
+        <div className='proyect__card'>
+          <h2 className='card__title'>Ley de ohm</h2>
+          <img src="" alt="" />
+          <p></p>
+          <button>Probar</button>
+          <button>{"codigo</>"}</button>
+        </div>
+
+      </div>
+      </div>
+    </section>
   )
 }
 
