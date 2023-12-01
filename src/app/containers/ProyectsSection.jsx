4dbea6ffbdd020752c7935a6c706f41812729c71
asdjosegarcia <asdjosegarcia@gmail.com>
2023-12-01 19:58:21 -0300

    add jigsaw to proyects fix proyectsSection styles

--- src/app/containers/ProyectsSection.jsx
+++ src/app/containers/ProyectsSection.jsx
@@ -10,18 +10,39 @@ const ProyectsSection = () => {
           <div className='proyect__card'>
             {/* ley de hom */}
             <h2 className='card__title'>Ley de ohm</h2>
-            <img src="/img/ley-de-ohm_preview.jpg" alt="" />
+            <img src="/img/ley-de-ohm_preview.jpg" layout="fill" alt="" />
             <div className='proyect__skills__container'>
               <p className='proyect__skills'>JS</p>
               <p className='proyect__skills'>HTML</p>
               <p className='proyect__skills'>CSS</p>
               <p className='proyect__skills'>Webpack</p>
             </div>
-            <p>Este proyecto es una calculadora de la ley de ohm,<br/> suele ser muy util en proyectos de electronica.</p>
+            <p>Este proyecto es una calculadora de la ley de ohm,<br /> suele ser muy util en proyectos de electronica.</p>
             <a href="https://asdjosegarcia.github.io/ley-de-ohm/" target="_blank">
               <button className='card__button'>{"Probar  >>"}</button>
             </a>
             <a href="https://github.com/asdjosegarcia/ley-de-ohm/" target="_blank">
+              <button className='card__button'>{"Codigo  </>"}</button>
+            </a>
+          </div>
+          <div className='proyect__card'>
+            {/* ley de hom */}
+            <h2 className='card__title'>Jigsaw-puzzle</h2>
+            <img src="/img/jigsaw-preview.jpg"  alt="" />
+            <div className='proyect__skills__container'>
+              <p className='proyect__skills'>HTML</p>
+              <p className='proyect__skills'>CSS</p>
+              <p className='proyect__skills'>JS</p>
+              <p className='proyect__skills'>Nextjs</p>
+              <p className='proyect__skills'>Webpack</p>
+              <p className='proyect__skills'>PostgresSQL</p>
+              <p className='proyect__skills'>Prisma</p>
+            </div>
+            <p>Este proyecto es rompecabezas basado en el juego 15,<br /> suele ser muy util en proyectos de electronica.</p>
+            <a href="https://jigsaw-asdjosegarcia.vercel.app/" target="_blank">
+              <button className='card__button'>{"Probar  >>"}</button>
+            </a>
+            <a href="https://github.com/asdjosegarcia/jigsaw-puzzle/" target="_blank">
               <button className='card__button'>{"Codigo  </>"}</button>
             </a>
           </div>
